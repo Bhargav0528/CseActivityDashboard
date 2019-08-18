97b9f57bedefde3333ac916be31131d0824d0189
aditya-100 <52001350+aditya-100@users.noreply.github.com>
2019-08-18 22:55:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import './App.css';
+import Root from './components/root';
+import {Link, BrowserRouter,Switch, Route} from 'react-router-dom';
+import fire from './config/firebaseConfig';
+import {Nav, Navbar} from 'react-bootstrap';
+import ProjDashBoard from './components/ProjDashBoard';
+import LandingPage from './components/LandingPage';
+import Profile from './components/Profile';
+import Authentication from './components/Authentication';
+import ProjectDetails from './components/ProjectDetails';
+import APPjs from './InternshipPortal/App';
+import {MainPage, NewInternship, NewInternshipLink, MiniTestForm, Internship, SendNotification, EditInternship, StudentFeedbackCompany, SkillSearch} from './InternshipPortal/adminDashboard.js';
+import MinitestConduct from './InternshipPortal/minitestStudent.js';
+import {StudentDashboard} from './InternshipPortal/studentDashboard';
+import {FeedbackForm} from './InternshipPortal/feedbackForm';
+import {SubmitProfile, ProfileSubmitted} from './InternshipPortal/studentProfile';
+
+class App extends Component {
+    state = {user:null, fullName:""}
+
+componentDidMount(){
+    this.authListener();
+}
+authListener(){
+
+    fire.auth().onAuthStateChanged((user) =>{
+      //console.log(user);
+      if(user){
+		if(!user.isAnonymous){
+        fire.database().ref(`users/${fire.auth().currentUser.uid}/`).on('value', (snap)=>{
+            this.setState({user:fire.auth().currentUser.uid, fullName:snap.val().fullName});
+        })
+		}
+		else{this.setState({user:null});}
+        
+        //localStorage.setItem('user',user.uid);
+      }else{
+        this.setState({user:null});
+        //localStorage.removeItem('user');
+
+      }
+      });
+    
+  }
+
+  profileCheck()
+  {
+      if(this.state.user!== null)
+      {
+        return <Link style={{fontSize:16, marginRight:20, color:'#fff'}} to="/profile">{this.state.fullName}</Link>
+      }
+
+      else
+      {
+        return <Link style={{fontSize:16, marginRight:20, color:'#fff'}} to="/auth">Login</Link>
+      }
+  }
+
+  render() {
+    return (
+      <div className="nav-bar">
+        <Navbar bg="dark" variant="dark" className="navbar">
+        <img src="https://img.collegepravesh.com/2018/10/DSI-Bangalore-Logo.png" style={{width:'40px', height:'40px', marginRight:'20px'}} />
+        <Navbar.Brand href="#home">CSE Dashboard</Navbar.Brand>
+        <Nav className="ml-auto">
+        <div>
+        <Link style={{fontSize:16, marginRight:20, color:'#fff'}} to="/projects">Projects</Link>
+        <Link style={{fontSize:16, marginRight:20, color:'#fff'}} to="/Internship">Internship</Link>
+        {this.profileCheck()}
+        <Link style={{fontSize:16, marginRight:20, color:'#fff'}}to="#">Pricing</Link>
+        </div>
+        </Nav>
+        </Navbar>
+        <div style={{width:"100%"}}>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/projects" component={ProjDashBoard} />
+          <Route path="/users" component={Profile} />
+          <Route path="/auth" component={Authentication} />
+          <Route path="/project/:ids" component={ProjectDetails} />
+        <Route exact path="/Internship" component={APPjs}/>
+	<Route exact path="/studentdashboard" component={StudentDashboard}/>
+	<Route path="/admindashboard" component={MainPage}/>
+	<Route path="/createminitest" component={MiniTestForm}/>
+	<Route path="/internship" component={Internship}/>
+	<Route path="/newinternshiplink" component={NewInternshipLink}/>
+	<Route path="/sendnotification" component={SendNotification}/>
+	<Route exact path="/sprofile" component={SubmitProfile}/>
+	<Route path="/submitted" component={ProfileSubmitted}/>
+	<Route path="/studentdashboard/feedback" component={FeedbackForm}/>
+	<Route path="/minitest" component={MinitestConduct}/>
+		<Route path="/editinternship" component={EditInternship}/>
+		<Route path="/skillsearchinternship" component={SkillSearch}/>
+		<Route path="/newinternship" component={NewInternship}/>
+		<Route path="/studentfeedbackinternship" component={StudentFeedbackCompany}/>
+        </Switch>
+        </div>
+</div>
+    );
+  }
+}
+
+export default App;
